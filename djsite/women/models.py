7acd9627f01ac9a34a35060676ddc0460872171f
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-05-02 23:34:42 +0300

    Cusomize admin panel with list_display, filters and search

--- djsite/women/models.py
+++ djsite/women/models.py
@@ -16,11 +16,21 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Famous women'
+        verbose_name_plural = 'Famous women'
+        ordering = ['-time_create', 'title']
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Category')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['id']
